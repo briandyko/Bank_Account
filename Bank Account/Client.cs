b816167606c3fc5edc5646cba804def3c8cfcc17
briandyko <briandyko@gmail.com>
2017-10-13 11:42:46 -0400

    client class defined

---
+++ Bank Account/Client.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Account
+{
+    class Client
+    {
+        private string userName;
+        private string bankIdentificationNumber;
+        private string userBirthDate;
+        
+        public string UserName
+        {
+            get { return this.userName; }
+            set { this.userName = value; }   // do i need sets for any of these three values? I don't really want them setting it, at least not yet (stretch task).
+        }
+
+        public string BankIdentificationNumber
+        {
+            get { return this.bankIdentificationNumber; }
+            set { this.bankIdentificationNumber = value; }
+        }
+
+        public string UserBirthDate
+        {
+            get { return this.userBirthDate; }
+            set { this.userBirthDate = value; }
+        }
+
+        public Client()
+        {
+            userName = "Grover Cleveland";                // for now this can be hardcoded in, but maybe I'll need to console.readline it for the stretch task.
+            bankIdentificationNumber = "Customer ID: 4931bvX2984z";
+            userBirthDate = "03 / 18 / 1837";
+
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine("\n" + userName + "\n" + bankIdentificationNumber + "\n" + userBirthDate + "\n Thanks for being a loyal customer " + userName + "!");
+        }
+    }
+}
--- Bank Account/Program.cs
+++ Bank Account/Program.cs
@@ -10,6 +10,23 @@ namespace Bank_Account
     {
         static void Main(string[] args)
         {
+            //Generic Coroporate Bank Welcome Screen
+            Console.WriteLine("Welcome to Whatever Bank! \n Please type in the number of your desired action below. \n");         
+            Console.WriteLine("1. View Client Information");
+            Console.WriteLine("2. View Account Balance");
+            Console.WriteLine("3. Deposit Funds");
+            Console.WriteLine("4. Withdraw Funds");
+            Console.WriteLine("5. Exit");
+            int userChoice = int.Parse(Console.ReadLine());
+
+            Client groverCleveland = new Client();
+
+            if (userChoice == 1)
+            {
+                groverCleveland.GetInfo();
+            }
+
+
         }
     }
 }
